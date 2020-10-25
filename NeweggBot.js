f305e557fece45bdc3199e363487e1fae70103f0
ArdonnieIV <44914469+ArdonnieIV@users.noreply.github.com>
2020-10-25 00:05:10 -0400

    Chrome rtx bot
    This version only works with 1 product number at a time. I noticed the original bot would get stuck along the checkout process because Newegg has multiple variations of each page. So I added multiple selectors for each step. I believe this bot will currently work just fine but it's possible I missed a variation of one of the pages.
    
    ps.
    Do not include -PUPPETEER_PRODUCT=firefox in the ndm install puppeteer command if you want to run the bot on chrome.

--- NeweggBot.js
+++ NeweggBot.js
@@ -1,78 +1,67 @@
 const puppeteer = require('puppeteer')
 const config = require('./config.json')
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 async function report (log) {
 	currentTime = new Date();
 	console.log(currentTime.toString().split('G')[0] + ': ' + log)
 }
-async function check_cart (page) {
-	await page.waitForTimeout(250)
-	try {
-		await page.waitForSelector('span.amount' , { timeout: 1000 })
-		var element = await page.$('span.amount')
-		var text = await page.evaluate(element => element.textContent, element);
-		if (parseInt(text.split('$')[1]) > config.price_limit) {
-			await report("Price exceeds limit, removing from cart")
-			var button = await page.$$('button.btn.btn-mini');
-			while (true) {
-				try {
-					await button[1].click()
-				} catch (err) {
-					break
-				}
-			}
-			return false
-		}
-		await report("Card added to cart, attempting to purchase")
-		return true
-	} catch (err) {
-		await report("Card not in stock")
-		await page.waitForTimeout(config.refresh_time * 1000)
-		return false
-	}
-}
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 async function run () {
+
 	await report("Started")
+
 	const browser = await puppeteer.launch({
         	headless: false,
-			product: 'firefox',
+			product: 'chrome',
         	defaultViewport: { width: 1366, height: 768 }
-    	})
+		})
+		
     const page = await browser.newPage()
 	
     while (true) {
+
 		await page.goto('https://secure.newegg.com/NewMyAccount/AccountLogin.aspx?nextpage=https%3a%2f%2fwww.newegg.com%2f' , {waitUntil: 'load' })
+
 		if (page.url().includes('signin')) {
+
 			await page.waitForSelector('button.btn.btn-orange')
 			await page.type('#labeled-input-signEmail', config.email)
 			await page.click('button.btn.btn-orange')
 			await page.waitForTimeout(1500)
+
 			try {
 				await page.waitForSelector('#labeled-input-signEmail', {timeout: 500})
-			} catch (err) {
+			} 
+			catch (err) {
 				try {
 					await page.waitForSelector('#labeled-input-password' , {timeout: 2500})
 					await page.waitForSelector('button.btn.btn-orange')
 					await page.type('#labeled-input-password', config.password)
 					await page.click('button.btn.btn-orange')
 					await page.waitForTimeout(1500)
+
 					try {
 						await page.waitForSelector('#labeled-input-password', {timeout: 500})
-					} catch (err) {
+					} 
+					catch (err) {
 						break
 					}
-				} catch (err) {
+				} 
+				catch (err) {
 					report("Manual authorization code required by Newegg.  This should only happen once.")
-					while (page.url().includes('signin'))
-					{
+					while (page.url().includes('signin')) {
 						await page.waitForTimeout(500)
 					}
 					break
 				}
 			}
-		} else if (page.url().includes("areyouahuman")) {
+		} 
+
+		else if (page.url().includes("areyouahuman")) {
 			await page.waitForTimeout(1000)
 		}
 	}
@@ -82,58 +71,139 @@ async function run () {
 
 	while (true)
 	{
+
 		try {
-			await page.goto('https://secure.newegg.com/Shopping/AddtoCart.aspx?Submit=ADD&ItemList=' + config.item_number, { waitUntil: 'load' })
-			if (page.url().includes("ShoppingCart")) {
-				var check = await check_cart(page)
-				if (check) {
-					break
-				}
-			} else if (page.url().includes("ShoppingItem")) {
+
+			if (!(page.url().includes("ShoppingItem"))) { // incase of loop, still only add 1 to cart
+				await page.goto('https://secure.newegg.com/Shopping/AddtoCart.aspx?Submit=ADD&ItemList=' + config.item_number, { waitUntil: 'load' })
+			}
+
+			else if (page.url().includes("ShoppingItem")) { // item is in cart
+
 				await page.goto('https://secure.newegg.com/Shopping/ShoppingCart.aspx', { waitUntil: 'load' })
-				var check = await check_cart(page)
-				if (check){
+
+				try {
+					await page.waitForSelector('[class="button button-primary has-icon-right"]', {timeout: 500})
+					await page.click('[class="button button-primary has-icon-right"]')
 					break
-				}
-			} else if (page.url().includes("areyouahuman")) {
+				} 
+				catch (err) {}
+
+				try {
+					await page.waitForSelector('[class="button button-primary button-override has-icon-right"]', {timeout: 500})
+					await page.click('[class="button button-primary button-override has-icon-right"]')
+					break
+				} 
+				catch (err) {}
+				
+				try {
+					await page.waitForSelector('[class="btn btn-primary btn-wide"]', {timeout: 500})
+					await page.click('[class="btn btn-primary btn-wide"]')
+					break
+				} 
+				catch (err) {}
+			} 
+
+			else if (page.url().includes("areyouahuman")) {
 				await page.waitForTimeout(1000)
 			}
-		} catch (err) {
+		} 
+		catch (err) {
 			continue
 		}
 	}
-	try {
-		await page.goto('javascript:attachDelegateEvent((function(){Biz.GlobalShopping.ShoppingCart.checkOut(\'True\')}))', {timeout: 500})
-	} catch (err) {
-	}
+
+	await page.waitForTimeout(1500)
+
+	// CONTINUE TO PAYMENT
+	while(true) {
+		try {
+			await page.waitForSelector('#app > div > section > div > div > form > div.row-inner > div.row-body > div > div:nth-child(2) > div > div.checkout-step-action > button', {timeout: 500})
+			await page.click('#app > div > section > div > div > form > div.row-inner > div.row-body > div > div:nth-child(2) > div > div.checkout-step-action > button')
+			break
+		} catch (err) {
+		}
 	
+		try {
+			await page.waitForSelector('#orderSummaryPanelAndPayment > div > div.additional-info-groupbox > div > div > a', {timeout: 500})
+			await page.click('#orderSummaryPanelAndPayment > div > div.additional-info-groupbox > div > div > a')
+			break
+		} catch (err) {
+		}
+	}
+
+	await page.waitForTimeout(1500)
+
+	// ENTER CVV
 	while (true) {
+
 		try {
 			await page.waitForSelector('#cvv2Code' , {timeout: 500})
 			await page.type('#cvv2Code', config.cv2)
 			break
-		} catch (err) {
-		}
+		} 
+		catch (err) {}
+
 		try {
 			await page.waitForSelector('#creditCardCVV2' , {timeout: 500})
 			await page.type('#creditCardCVV2', config.cv2)
 			break
-		} catch (err) {
-		}
+		} 
+		catch (err) {}
+
+		try {
+			await page.waitForSelector('#app > div > section > div > div > form > div.row-inner > div.row-body > div > div:nth-child(3) > div > div.checkout-step-body > div > div.checkout-tabs-wrap.margin-top > div.checkout-tab-content.is-active > div.item-cells-wrap.border-cells.tile-cells.three-cells.expulsion-one-cell.checkout-card-cells > div:nth-child(1) > div > label > div.retype-security-code > input' , {timeout: 500})
+			await page.type('#app > div > section > div > div > form > div.row-inner > div.row-body > div > div:nth-child(3) > div > div.checkout-step-body > div > div.checkout-tabs-wrap.margin-top > div.checkout-tab-content.is-active > div.item-cells-wrap.border-cells.tile-cells.three-cells.expulsion-one-cell.checkout-card-cells > div:nth-child(1) > div > label > div.retype-security-code > input', config.cv2)
+			break
+		} 
+		catch (err) {}
 	}
 
-	try {
-		await page.waitForSelector('#term' , {timeout: 5000})	
-		await page.click('#term')
-	} catch (err) {
+	await page.waitForTimeout(1500)
+
+	// CONBTINUE TO ORDER REVIEW
+	while (true) {
+
+		try {
+			await page.waitForSelector('#btnCreditCard > a' , {timeout: 5000})	
+			await page.click('#btnCreditCard > a')
+			break
+		} catch (err) {}
+
+		try {
+			await page.waitForSelector('#app > div > section > div > div > form > div.row-inner > div.row-body > div > div:nth-child(3) > div > div.checkout-step-action > button' , {timeout: 5000})	
+			await page.click('#app > div > section > div > div > form > div.row-inner > div.row-body > div > div:nth-child(3) > div > div.checkout-step-action > button')
+			break
+		} catch (err) {}
 	}
 
-	if (config.auto_submit == 'true') {
-		await page.click('#SubmitOrder')
+	await page.waitForTimeout(1500)
+
+	// PLACE ORDER
+	while (config.auto_submit) {
+
+		try {
+			await page.waitForSelector('#btnCreditCard' , {timeout: 5000})	
+			await page.click('#btnCreditCard')
+			break
+		} catch (err) {}
+
+		try {
+			await page.waitForSelector('#SubmitOrder' , {timeout: 5000})	
+			await page.click('#SubmitOrder')
+			break
+		} catch (err) {}
+
+		try { // incase of UPS suggested billing address
+			await page.waitForSelector('#BillingForm > div > div.recommend > a' , {timeout: 5000})	
+			await page.click('#BillingForm > div > div.recommend > a')
+			break
+		} catch (err) {}
 	}
+
 	await report("Completed purchase")
-    	//await browser.close()
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 run()
